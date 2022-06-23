94fa98f0ebe7bd325c700efaf89a285883e0d912
Sourabh Darwai <sdarwai15@gmail.com>
2022-06-23 20:53:35 +0530

    added data models

---
+++ backend/models/ChatModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Config = require("../config/config");
+
+const chatSchema = mongoose.Schema(
+	{
+		chatName: { type: String, trim: true },
+		isGroupChat: { type: Boolean, default: false },
+		users: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: Config.collection_names.User,
+			},
+		],
+		latestMessage: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: Config.collection_names.Message,
+		},
+		groupAdmin: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: Config.collection_names.User,
+		},
+	},
+	{ timestamps: true }
+);
+
+const ChatModel = mongoose.model(Config.collection_names.Chat, chatSchema);
+
+module.exports = ChatModel;
---
+++ backend/models/UserModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Config = require("../config/config");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema(
+	{
+		name: { type: "String", required: true },
+		email: { type: "String", unique: true, required: true },
+		password: { type: "String", required: true },
+		pic: {
+			type: "String",
+			required: true,
+			default:
+				"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+	},
+	{ timestaps: true }
+);
+
+userSchema.pre("save", async function (next) {
+	// if password is not modified, skip this function
+	if (this.isModified("password")) {
+		// hash the password before saving
+		this.password = await bcrypt.hash(this.password, 10);
+	}
+	next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+	// compare the password with the hashed password
+	return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAuthToken = function () {
+	// generate a token for the user
+	return jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+};
+
+const userModel = mongoose.model(Config.collection_names.User, userSchema);
+
+module.exports = userModel;
