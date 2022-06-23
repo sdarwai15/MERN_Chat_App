91ceeb054751c82504a8beafd14c6cefd3bda5be
Sourabh Darwai <sdarwai15@gmail.com>
2022-06-23 23:08:32 +0530

    formatting

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -3,25 +3,32 @@ const Config = require("../config/config");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema(
-	{
-		name: { type: "String", required: true },
-		email: { type: "String", unique: true, required: true },
-		password: { type: "String", required: true },
-		pic: {
-			type: "String",
-			required: true,
-			default:
-				"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-		},
-		isAdmin: {
-			type: Boolean,
-			required: true,
-			default: false,
-		},
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
 	},
-	{ timestaps: true }
-);
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	pic: {
+		type: String,
+		required: true,
+		default:
+			"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+	},
+	isAdmin: {
+		type: Boolean,
+		required: true,
+		default: false,
+	},
+});
 
 userSchema.pre("save", async function (next) {
 	// if password is not modified, skip this function
