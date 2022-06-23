38ffe19151a244847b1bf01a17dae93bee37b9bb
Sourabh Darwai <sdarwai15@gmail.com>
2022-06-23 23:08:48 +0530

    implemented auth.js

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,42 @@
+const UserModel = require("../models/UserModel");
+const ApiError = require("../errors/handler");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+	async isAuthenticated(req, res, next) {
+		try {
+			const token = req.cookies.token;
+
+			if (!token) {
+				// if no token, return error
+				res.status(401).json({
+					description: "Authorization token is required in headers",
+					errors: [
+						{
+							errorCode: 401,
+							field: "Authorization",
+							message: "Please Login first",
+						},
+					],
+				});
+				
+				return;
+			}
+
+			// verify token
+			const decoded = jwt.verify(token, process.env.JWT_SECRET);
+			// if token is valid, find user by id in db
+			const user = await UserModel.findById(decoded._id);
+			if (!user) {
+				// if user not found, return error
+				return next(ApiError.unauthorized("Please Login first"));
+			}
+			// if user found, set user to req.user
+			req.user = user;
+			next();
+		} catch (error) {
+			// console.log(error);
+			next(error);
+		}
+	},
+};
