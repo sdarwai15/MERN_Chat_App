5ba7d916d30b20517048d6b308d66decdb86174d
Sourabh Darwai <sdarwai15@gmail.com>
2022-06-24 09:53:42 +0530

    built APIs for Chats

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const { isAuthenticated } = require("../middlewares/auth");
+const {
+	accessChat,
+	fetchChats,
+	createGroupChat,
+	renameGroup,
+	removeFromGroup,
+    addToGroup,
+} = require("../controllers/ChatController");
+
+/**
+ * @desc access chat
+ * @route POST /chat
+ * @access Private
+ **/
+
+router.route("/").post(isAuthenticated, accessChat);
+
+/**
+ * @desc fetch chat
+ * @route GET /chat
+ * @access Private
+ **/
+
+router.route("/").get(isAuthenticated, fetchChats);
+
+/**
+ * @desc creates a group chat
+ * @route GET /chat/group
+ * @access Private
+ **/
+
+router.route("/group").get(isAuthenticated, createGroupChat);
+
+/**
+ * @desc renames a group chat
+ * @route GET /chat/rename
+ * @access Private
+ **/
+
+router.route("/rename").patch(isAuthenticated, renameGroup);
+
+/**
+ * @desc removes a user from a group chat
+ * @route GET /chat/groupremove
+ * @access Private
+ **/
+
+router.route("/groupremove").patch(isAuthenticated, removeFromGroup);
+
+/**
+ * @desc adds a user to a group chat
+ * @route GET /chat/groupadd
+ * @access Private
+ **/
+
+router.route("/groupadd").patch(isAuthenticated, addToGroup);
+
+module.exports = router;
