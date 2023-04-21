73000825b45c7f8e9b072f045395492a80d8d786
sdarwai15 <sdarwai15@gmail.com>
2023-04-21 22:57:25 +0530

    enhancements

--- frontend/src/Components/ChatBox/SingleChat.jsx
+++ frontend/src/Components/ChatBox/SingleChat.jsx
@@ -21,7 +21,7 @@ import animationData from "../../animations/typing-animation.json";
 
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:4000";
+const ENDPOINT = "https://chit-chat-sula.onrender.com";
 let socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
